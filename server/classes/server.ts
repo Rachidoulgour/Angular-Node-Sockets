e88724b28b86194047fb57d6e80b198cca4827fa
rachid <olgor@hotmail.com>
2021-01-02 22:53:15 +0100

    socket added

--- server/classes/server.ts
+++ server/classes/server.ts
@@ -1,19 +1,25 @@
 
 import express from 'express';
 import { SERVER_PORT } from '../global/environment';
+import socketIO from 'socket.io';
+import http from 'http';
 
 
 export default class Server{
 
     public app: express.Application;
     public port: number;
+    public io: socketIO.Server;
+    private httpServer: http.Server;
 
     constructor(){
         this.app = express();
         this.port = SERVER_PORT;
+        this.httpServer = new http.Server(this.app);
+        this.io = (socketIO as any)(this.httpServer);
     }
 
     start(callback: Function){
-        this.app.listen(this.port, callback());
+        this.httpServer.listen(this.port, callback());
     }
 }
